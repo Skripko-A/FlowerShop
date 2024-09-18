cb46392714a9a58e147ba4410bc2ac857d0a3ee0
a-skripko <skripkoae@gmail.com>
2024-09-18 11:52:27 +0400

    add catalog and order urls

--- FlowerShop/urls.py
+++ FlowerShop/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from FlowerShop.views import show_main
+from FlowerShop.views import show_catalog, show_main, show_order
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', show_main, name='show_main'),
+    path('catalog', show_catalog, name='show_catalog'),
+    path('order', show_order, name='show_order')
 ]
--- FlowerShop/views.py
+++ FlowerShop/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def show_main(request):
     context={}
     return render(request, 'index.html', context)
+
+
+def show_catalog(request):
+    context={}
+    return render(request, 'catalog.html', context)
+
+
+def show_order(request):
+    context={}
+    return render(request, 'order.html', context)
